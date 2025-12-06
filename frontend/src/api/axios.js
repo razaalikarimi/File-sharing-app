19759bc21af8300256569e81c4397277032aa6fa
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2025-12-06 14:23:25 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://file-sharing-app-b.onrender.com",
+  baseURL: "https://file-sharing-app-b.onrender.com/api",
 });
 
 api.interceptors.request.use(
